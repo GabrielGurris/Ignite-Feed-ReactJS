0eab287d023fe03c310162549fe9f64760ea9bda
gabrielgurris <gabriel_gurris@outlook.com>
2023-07-23 20:22:35 -0300

    using States to change the values of variables

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,22 +1,29 @@
 import { format, formatDistanceToNow } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
+import { useState } from "react";
+
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 export function Post({ author, publishedAt, content }) {
-  const publishedDateFormatted = format(
-    publishedAt,
-    "d 'de' LLLL 'às' HH:mm'h'",
-    {
+  const [comments, setComments] = useState([1, 2]);
+
+  function handleCrateNewComment() {
+    event.preventDefault();
+
+    setComments([...comments, comments.length + 1]);
+  }
+
+  const publishedDateFormatted =  format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
       locale: ptBR,
-    }
-  );
+    });
 
   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
     locale: ptBR,
     addSuffix: true,
   });
+
   return (
     <article className={styles.post}>
       <header>
@@ -28,22 +35,29 @@ export function Post({ author, publishedAt, content }) {
           </div>
         </div>
 
-        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
           {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        {content.map(line => {
-          if (line.type === 'paragraph') {
+        {content.map((line) => {
+          if (line.type === "paragraph") {
             return <p>{line.content}</p>;
-          } else if (line.type === 'link') {
-            return <p><a href="#">{line.content}</a></p>
+          } else if (line.type === "link") {
+            return (
+              <p>
+                <a href="#">{line.content}</a>
+              </p>
+            );
           }
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCrateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea placeholder="Deixe um comentário" />
@@ -54,9 +68,9 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map((comment) => {
+          return <Comment />;
+        })}
       </div>
     </article>
   );
